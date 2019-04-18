98aa0d55ced19e99ae80367f04bd290b59a484cf
Sergey <immortalizedinhearts@gmail.com>
2019-04-18 22:39:45 +0300

    Fixed problem in test.py

--- test.py
+++ test.py
@@ -1,3 +1,5 @@
 def greetings():
-    return """Hello, my dear user!
-              Could you tell me about yourself?"""
+    print("""Hello, my dear user!
+              Could you tell me about yourself?""")
+
+greetings()
